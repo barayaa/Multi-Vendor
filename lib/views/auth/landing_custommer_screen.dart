4d482064801157d65e0210c238690e335a9f0d1c
mahamadou.baraya <your_email_address@example.com>
2022-09-08 23:52:41 +0200

    firebase integration

--- lib/views/auth/landing_custommer_screen.dart
+++ lib/views/auth/landing_custommer_screen.dart
@@ -1,89 +1,249 @@
 import 'package:flutter/material.dart';
 
-class LandingCustommerScreen extends StatelessWidget {
+class LandingCustommerScreen extends StatefulWidget {
   const LandingCustommerScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LandingCustommerScreen> createState() => _LandingCustommerScreenState();
+}
+
+class _LandingCustommerScreenState extends State<LandingCustommerScreen> {
+  final TextEditingController fullnameController = new TextEditingController();
+  final TextEditingController emailController = new TextEditingController();
+  final TextEditingController passwordController = new TextEditingController();
+  bool passwordVisible = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              "Create custommer account",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            IconButton(
-              icon: Icon(
-                Icons.person,
-                size: 35,
-                color: Colors.cyan,
-              ),
-              onPressed: () {},
-            )
-          ],
-        ),
-        Row(
-          children: [
-            CircleAvatar(
-              radius: 60,
-              backgroundColor: Colors.cyan,
-            ),
-            SizedBox(
-              width: 10,
-            ),
-            Column(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.cyan,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(
-                        14,
-                      ),
-                      topRight: Radius.circular(
-                        14,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Create custommer account",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                  ),
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.camera_alt,
-                    ),
-                  ),
+                    IconButton(
+                      icon: Icon(
+                        Icons.person,
+                        size: 35,
+                        color: Colors.cyan,
+                      ),
+                      onPressed: () {},
+                    )
+                  ],
                 ),
-                SizedBox(height: 10),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.cyan,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(
-                        14,
-                      ),
-                      topRight: Radius.circular(
-                        14,
-                      ),
+                Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 60,
+                      backgroundColor: Colors.cyan,
                     ),
-                  ),
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.photo,
+                    SizedBox(
+                      width: 10,
                     ),
-                  ),
+                    Column(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.cyan,
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(
+                                14,
+                              ),
+                              topRight: Radius.circular(
+                                14,
+                              ),
+                            ),
+                          ),
+                          child: IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.camera_alt,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.cyan,
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(
+                                14,
+                              ),
+                              topRight: Radius.circular(
+                                14,
+                              ),
+                            ),
+                          ),
+                          child: IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.photo,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
                 ),
+                SizedBox(
+                  height: 15,
+                ),
+                Column(
+                  children: [
+                    TextFormField(
+                      controller: fullnameController,
+                      decoration: InputDecoration(
+                        labelText: "Full-Name",
+                        hintText: "Enter your full name",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(
+                            15,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 15),
+                    TextFormField(
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        labelText: "Email",
+                        hintText: "Enter your email",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(
+                            15,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    TextFormField(
+                      controller: passwordController,
+                      obscureText: passwordVisible,
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              passwordVisible = !passwordVisible;
+                            });
+                          },
+                          icon: passwordVisible
+                              ? Icon(
+                                  Icons.visibility,
+                                )
+                              : Icon(
+                                  Icons.visibility_off,
+                                ),
+                        ),
+                        hintText: "Enter your Password",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(
+                            15,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        print(fullnameController);
+                        print(emailController);
+                        print(passwordController);
+                      },
+                      child: Container(
+                        width: MediaQuery.of(context).size.width - 40,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          color: Colors.cyan,
+                          borderRadius: BorderRadius.circular(
+                            15,
+                          ),
+                        ),
+                        child: Center(
+                          child: Text(
+                            'Sign up',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Already have an account ?',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w100,
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Login',
+                          ),
+                        ),
+                      ],
+                    ),
+                    Text(
+                      'Or',
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Create a seller Account ?',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w100,
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Sign up',
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                )
               ],
-            )
-          ],
-        )
-      ],
-    ));
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
